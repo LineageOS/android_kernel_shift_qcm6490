302508f8b06aea215ea7359373db24785ccb630b
wei.liu515 <wei.liu515@sim.com>
2024-03-07 16:21:05 +0800

    [shiftphone8][backlight][increase min bl brightness]
    R&D:01515
    
    Change-Id: Ic9c2da5683f56525ced6f86d8c1188700b03d068
    Reviewed-on: http://review4-sh.sim.com:8080/c/Android-HighPlat-Repositories/platform/vendor/opensource/display-drivers/+/57011
    Reviewed-by: <wei.liu515@sim.com>
    Reviewed-by: <qimeng.guo@sim.com>

--- msm/sde/sde_connector.c
+++ msm/sde/sde_connector.c
@@ -144,6 +144,9 @@ static int sde_backlight_device_update_status(struct backlight_device *bd)
 	if (!bl_lvl && brightness)
 		bl_lvl = 1;
 
+	if (bl_lvl && brightness)
+		bl_lvl += 39;
+
 	if (!c_conn->allow_bl_update) {
 		c_conn->unset_bl_level = bl_lvl;
 		return 0;
